1b4aa514addb1cde6a387172e0c6f413a2bcc33e
Ali Reza Ghasemi <alir.ghase@gmail.com>
2024-08-01 13:02:32 +0100

    initialized my repo and created react project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <h1>London Events Board</h1>
+      <h2>Discover Exciting Events and Activities in London!</h2>
+    </div>
+  )
+}
+
+export default App
